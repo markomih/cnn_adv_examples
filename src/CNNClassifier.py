5a07bda10c74c05d430aa2cac63016c49bff5208
Maki94 <marko.mihajlovic@elfak.rs>
2017-09-26 23:57:27 +0200

    RMSE added and other features

--- src/CNNClassifier.py
+++ src/CNNClassifier.py
@@ -51,7 +51,9 @@ class CNNClassifier:
 
         self.graph.adv_class_placeholder = tf.placeholder(dtype=tf.int32, name='adv_class_placeholder')
         # adv_loss = self.loss(logits, [self.graph.adv_class_placeholder])  # maybe [adv..]
-        self.graph.adv_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.graph.probs, labels=[self.graph.adv_class_placeholder], name='loss_adv')
+        self.graph.adv_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.graph.probs,
+                                                                             labels=[self.graph.adv_class_placeholder],
+                                                                             name='loss_adv')
         self.graph.adv_gradient = tf.gradients(self.graph.adv_loss, self.graph.images_placeholder, name='adv_gradient')
 
         summary = tf.summary.merge_all()
@@ -74,7 +76,8 @@ class CNNClassifier:
         for step in range(self.max_steps):
             start_time = time.time()
 
-            feed_dict = self.fill_feed_dict(self.dataset.data.train, self.graph.images_placeholder, self.graph.labels_placeholder,
+            feed_dict = self.fill_feed_dict(self.dataset.data.train, self.graph.images_placeholder,
+                                            self.graph.labels_placeholder,
                                             self.graph.keep_prob, self.dropout_prob)
             activations, loss_value = self.sess.run([train_op, self.graph.loss], feed_dict=feed_dict)
 
@@ -91,37 +94,91 @@ class CNNClassifier:
                 print('Training Data Eval:')
                 self.do_eval(eval_correct, self.dataset.data.train, self.dropout_prob)
                 print('Validation Data Eval:')
-                self.do_eval(eval_correct,  self.dataset.data.validation, 1.0)
+                self.do_eval(eval_correct, self.dataset.data.validation, 1.0)
                 print('Test Data Eval:')
                 self.do_eval(eval_correct, self.dataset.data.test, 1.0)
         save_path = saver.save(self.sess, self.restore_model_path)
         print('Model saved in file: %s' % save_path)
         # endregion
 
-    def generate_adversarial_examples(self, cls_target=3, noise_limit=2, step_size=(1.0/255.0), required_score=0.95):
-        for img, cls_source in zip(self.dataset.data.test.images[:10], self.dataset.data.test.labels[:10]):
+    # TODO one more function
+    # TODO bool variable for two types; source-target and
+    def generate_adversarial_examples_every_image(self, cls_target=3, noise_limit=2, step_size=(1.0 / 255.0), max_iterations=200, source_target=False, fast_sign=False, epochs=10):
+        accuracy = 0
+        for epoch in range(epochs):
+            for img, cls_source in zip(self.dataset.data.test.images, self.dataset.data.test.labels):
+                img = img.reshape(1, self.dataset.image_pixels)
+                noise = np.zeros((1, self.dataset.image_pixels))
+
+                for i in range(max_iterations):
+                    noisy_image = img + noise
+                    noisy_image = np.clip(noisy_image, 0.0, 1.0)
+
+                    predictions = self.sess.run(self.graph.probs, feed_dict={
+                        self.graph.images_placeholder: noisy_image.reshape(1, self.dataset.image_pixels),
+                        self.graph.keep_prob: 1.0}).reshape(-1)
+
+                    if np.argmax(predictions) != cls_target if source_target else np.argmax(predictions) == cls_source:
+                        feed_dict = {self.graph.images_placeholder: noisy_image.reshape(1, self.dataset.image_pixels),
+                                     self.graph.adv_class_placeholder: cls_target, self.graph.keep_prob: 1.0}
+                        pred, grad = self.sess.run([self.graph.probs, self.graph.adv_gradient], feed_dict=feed_dict)
+                        pred, grad = pred[0], grad[0]
+
+                        if fast_sign:
+                            noise -= step_size * np.sign(grad)
+                        else:
+                            noise -= 2 * step_size * grad / max(np.abs(grad.max()), np.abs(grad.min()))
+
+                        # if False:  noise -= step_size * grad  # 10^7
+
+                        noise = np.clip(noise, -noise_limit, noise_limit)
+                    else:
+                        # self.plot(img, noise, noisy_image, cls_source, cls_target, predictions)
+                        accuracy += 1
+                        break
+
+    def generate_adversarial_examples(self, cls_target=3, noise_limit=.2, step_size=(1.0 / 255.0), max_iterations=200,
+                                      source_target=False, fast_sign=False):
+        adv_accuracy, same_classes, MSE = 0, 0, []
+        N = 100
+        for img, cls_source in zip(self.dataset.data.test.images[:N], self.dataset.data.test.labels[:N]):
+            if cls_source == cls_target:
+                same_classes += 1
+                continue
+
             img = img.reshape(1, self.dataset.image_pixels)
             noise = np.zeros((1, self.dataset.image_pixels))
 
-            for i in range(1000):
+            for i in range(max_iterations):
                 noisy_image = img + noise
                 noisy_image = np.clip(noisy_image, 0.0, 1.0)
 
-                predictions = self.sess.run(self.graph.probs, feed_dict={self.graph.images_placeholder: noisy_image.reshape(1, self.dataset.image_pixels), self.graph.keep_prob: 1.0}).reshape(-1)
-                print('iteration %s' % i, np.argmax(predictions), cls_source)
-                if np.argmax(predictions) != cls_target:  # if np.argmax(predictions[0]) == cls_source:
-                    feed_dict = {self.graph.images_placeholder: noisy_image.reshape(1, self.dataset.image_pixels), self.graph.adv_class_placeholder: cls_target, self.graph.keep_prob: 1.0}
+                predictions = self.sess.run(self.graph.probs, feed_dict={
+                    self.graph.images_placeholder: noisy_image.reshape(1, self.dataset.image_pixels),
+                    self.graph.keep_prob: 1.0}).reshape(-1)
+                # print('iteration %s' % i, np.argmax(predictions), cls_source)
+
+                if np.argmax(predictions) != cls_target if source_target else np.argmax(predictions) == cls_source:
+                    feed_dict = {self.graph.images_placeholder: noisy_image.reshape(1, self.dataset.image_pixels),
+                                 self.graph.adv_class_placeholder: cls_target, self.graph.keep_prob: 1.0}
                     pred, grad = self.sess.run([self.graph.probs, self.graph.adv_gradient], feed_dict=feed_dict)
                     pred, grad = pred[0], grad[0]
 
-                    noise -= step_size * np.sign(grad)
-                    if False:
-                        noise -= step_size * grad  # 10^7
+                    if fast_sign:
+                        noise -= step_size * np.sign(grad)
+                    else:
+                        noise -= 2 * step_size * grad / max(np.abs(grad.max()), np.abs(grad.min()))
+
+                    # if False:  noise -= step_size * grad  # 10^7
 
                     noise = np.clip(noise, -noise_limit, noise_limit)
                 else:
-                    self.plot(img, noise, noisy_image, cls_source, cls_target, predictions)
+                    # self.plot(img, noise, noisy_image, cls_source, cls_target, predictions)
+                    adv_accuracy += 1
+                    MSE.append(noise**2)
                     break
+        m = np.sqrt(np.sum(MSE)/(len(MSE)-1))
+        print("RMSE=", m, "\tadv_accuracy=", adv_accuracy/(N-same_classes), '\t\t\t\tstep size=', step_size, "\tsource-target=%s\t" % source_target, '\tfast_sign=%s' % fast_sign)
 
         writer = tf.summary.FileWriter(self.log_dir)
         writer.add_graph(tf.get_default_graph())
@@ -139,7 +196,8 @@ class CNNClassifier:
         ax3.imshow(img.reshape(self.dataset.image_size, self.dataset.image_size), cmap='gray')
         fig.sca(ax3)
 
-        ax4.imshow((noise + 0.5).reshape(self.dataset.image_size, self.dataset.image_size), cmap='gray')
+        noise += .5
+        ax4.imshow(noise.reshape(self.dataset.image_size, self.dataset.image_size), cmap='gray')
 
         barlist = ax2.bar(range(10), topprobs)
         if cls_target in topk:
@@ -173,7 +231,8 @@ class CNNClassifier:
         num_examples = steps_per_epoch * self.batch_size
 
         for step in range(steps_per_epoch):
-            feed_dict = self.fill_feed_dict(data_set, self.graph.images_placeholder, self.graph.labels_placeholder, self.graph.keep_prob, dropout_prob)
+            feed_dict = self.fill_feed_dict(data_set, self.graph.images_placeholder, self.graph.labels_placeholder,
+                                            self.graph.keep_prob, dropout_prob)
             true_count += self.sess.run(eval_correct, feed_dict=feed_dict)
         precision = float(true_count) / num_examples
         print('Num examples: %d  Num correct: %d  Precision @ 1: %0.04f' % (num_examples, true_count, precision))
--- src/main.py
+++ src/main.py
@@ -2,4 +2,13 @@ from CNNClassifier import CNNClassifier
 
 cnn = CNNClassifier()
 cnn.restore_model()
-cnn.generate_adversarial_examples()
+cnn.generate_adversarial_examples(fast_sign=False, source_target=False, step_size=1./255.)
+cnn.generate_adversarial_examples(fast_sign=False, source_target=True, step_size=1./255.)
+cnn.generate_adversarial_examples(fast_sign=True, source_target=False, step_size=1./255.)
+cnn.generate_adversarial_examples(fast_sign=True, source_target=True, step_size=1./255.)
+
+cnn.generate_adversarial_examples(fast_sign=False, source_target=False, step_size=10./255.)
+cnn.generate_adversarial_examples(fast_sign=False, source_target=True, step_size=10./255.)
+cnn.generate_adversarial_examples(fast_sign=True, source_target=False, step_size=10./255.)
+cnn.generate_adversarial_examples(fast_sign=True, source_target=True, step_size=10./255.)
+
